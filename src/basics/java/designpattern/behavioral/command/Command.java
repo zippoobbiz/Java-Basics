9a3030bd15dc748732a13e822b920663e8b01a1a
Xiaoduo <zippoobbiz@gmail.com>
2016-10-06 17:18:43 +1100

    Command pattern

---
+++ src/basics/java/designpattern/behavioral/command/Command.java
@@ -0,0 +1,5 @@
+package basics.java.designpattern.behavioral.command;
+
+public interface Command {  
+    public void exe();  
+}  
---
+++ src/basics/java/designpattern/behavioral/command/Receiver.java
@@ -0,0 +1,7 @@
+package basics.java.designpattern.behavioral.command;
+
+public class Receiver {  
+    public void action(){  
+        System.out.println("command received!");  
+    }  
+}  
