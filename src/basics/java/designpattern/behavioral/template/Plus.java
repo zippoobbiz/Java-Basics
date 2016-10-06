5eed52acaebdcd45d27a6e27f0b293e0422d50f7
Xiaoduo <zippoobbiz@gmail.com>
2016-10-06 23:46:00 +1100

    Template pattern

---
+++ src/basics/java/designpattern/behavioral/template/Plus.java
@@ -0,0 +1,9 @@
+package basics.java.designpattern.behavioral.template;
+
+public class Plus extends AbstractCalculator {  
+	  
+    @Override  
+    public int calculate(int num1,int num2) {  
+        return num1 + num2;  
+    }  
+} 
---
+++ src/basics/java/designpattern/behavioral/template/StrategyTest.java
@@ -0,0 +1,11 @@
+package basics.java.designpattern.behavioral.template;
+
+public class StrategyTest {  
+	  
+    public static void main(String[] args) {  
+        String exp = "8+8";  
+        AbstractCalculator cal = new Plus();  
+        int result = cal.calculate(exp, "\\+");  
+        System.out.println(result);  
+    }  
+}  
